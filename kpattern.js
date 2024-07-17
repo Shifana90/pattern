480e232bc1e4cb154962ba9aa5fe4fc8a39a0adc
Shifana90 <shifanashifu198@gmail.com>
2024-07-17 13:10:44 +0530

    first commit

---
+++ kpattern.js
@@ -0,0 +1,14 @@
+for(i=1;i<=5;i++){
+    var pattern = ""
+    for(j=i;j<=5;j++){
+        pattern+='* '
+    }
+     console.log(pattern)
+}
+for(i=1;i<=5;i++){
+    var pattern = ""
+    for(j=1;j<=i;j++){
+        pattern+='* '
+    }
+     console.log(pattern)
+}
